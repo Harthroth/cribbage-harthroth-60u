72c812afac4989f6f1fae6d2da076828e13b5c2c
Harthroth <38116988+Harthroth@users.noreply.github.com>
2021-11-09 12:21:36 -0600

    Update Cribbage.py

--- Cribbage.py
+++ Cribbage.py
@@ -67,11 +67,12 @@ class Crib_Functions:
         response = requests.get("https://deckofcardsapi.com/api/deck/"+self.get_deck_ID+"/draw/?count=1")
         return json.loads(response)
 
-# For round play, placing cards in play pile
+# For round play, placing cards in play pile - pile_nameplay
     def add_card_to_play_piles(self, pile_name, card_name):
         response = requests.get("https://deckofcardsapi.com/api/deck/"+deck_ID+"/pile/"+pile_name+"play/add/?cards="+card_name)
         return json.loads(response)
 
+# End of play, put cards in new deck - pile_namescore
     def add_cards_to_score_pile(self, pile_name):
         response1 = requests.get("https://deckofcardsapi.com/api/deck/"+deck_ID+"/pile/"+pile_name+"play/list")
         x = json.loads(response1)
@@ -81,3 +82,73 @@ class Crib_Functions:
         card_list = card_list[:len(card_list)-1]
         response2 = requests.get("https://deckofcardsapi.com/api/deck/"+deck_ID+"/pile/"+pile_name+"score/add/?cards="+card_list)
         return json.loads(response2)
+
+class final_scoring:
+    def check_matching(self, pile):
+        total = 0
+        for x in pile:
+            for y in pile:
+                if x['value'] == y['value'] and x['code'] != y['code']:
+                    total+=2
+        return total
+
+    def check_fifteen(self, pile):
+        total = 0
+        for x in pile:
+            for y in pile:
+                if x['value'] + y['value'] == 15 and x['code'] != y['code']:
+                    total+=2
+        return total
+
+    def check_flush(self, pile):
+        total = 0
+        if len(pile) == 4:
+            if pile[0]['suit'] == pile[1]['suit'] == pile[2]['suit'] == pile[3]['suit']:
+                total+=4
+        elif len(pile) == 5:
+            if pile[0]['suit'] == pile[1]['suit'] == pile[2]['suit'] == pile[3]['suit'] == pile[4]['suit']:
+                total += 5
+        return total
+            
+
+    def check_runs(self, pile):
+        pass
+
+    def check_nubs(self, pile):
+        if len(pile) == 2:
+            if pile[0]['suit'] == pile[1]['suit']:
+                return 2
+
+    def check_all(self, pile):
+        fifteen = self.check_fifteen(pile)
+        flush = self.check_flush(pile)
+        matching = self.check_matching(pile)
+        nubs = self.check_nubs(pile)
+        runs = self.check_runs(pile)
+        return fifteen, flush, matching, nubs, runs
+    
+    def pile_value_change(self, pile):
+        for x in range(0, len(pile)):
+            if pile[x]['value'] == 'ACE':
+                pile[x]['value'] = '1'
+            elif pile[x]['value'] == 'JACK':
+                pile[x]['value'] = '11'
+            elif pile[x]['value'] == 'QUEEN':
+                pile[x]['value'] = '12'
+            elif pile[x]['value'] == 'KING':
+                pile[x]['value'] = '13'
+        return pile
+
+class play_scoring:
+    def check_fifteen(self, pile):
+        pass
+    def check_matching(self, pile):
+        pass
+    def check_runs(self, pile):
+        pass
+    def check_go(self, pile):
+        pass
+    def check_31(self, pile):
+        pass
+    def check_all(self, pile):
+        pass
