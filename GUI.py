70f1471ca63a4b394f5e7e1c25d997165d10a0d9
Luis <77930041+60u@users.noreply.github.com>
2021-11-09 17:41:55 -0600

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -4,11 +4,13 @@ import pygame.freetype  # Import the freetype module.
 
 pygame.init()
 
+# Setting up screen variables
+
 display_width = 1280
 display_height = 720
-
+crashed = False
 screen = pygame.display.set_mode((display_width,display_height))
-pygame.display.set_caption('Cribbage')
+pygame.display.set_caption('cribbage-harthroth-60u')
 
 BLACK = (0,0,0)
 WHITE = (255,255,255)
@@ -17,67 +19,92 @@ RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 TEST_COLOR = (199,36,177)
 
-crashed = False
+# Loading and editing assets
 
 GAME_FONT = pygame.freetype.Font('Assets\COMIC.TTF', 24)
-
 back_card_img = pygame.image.load('Assets\card_back.png')
 back_card_img = pygame.transform.scale(back_card_img, (172, 232))
 board_img = pygame.image.load('Assets\cribbage_board.png')
 board_img = pygame.transform.scale(board_img, (250, 700))
+empty_img = pygame.image.load('Assets\Empty.png')
+empty_img = pygame.transform.scale(empty_img, (156, 156))
+
+# Paints the default game screen
 
 def default_screen():
     screen.fill(GREEN)
     screen.blit(board_img, (1000, 0))
-    # Some misc text
-    deck_text, rect = GAME_FONT.render("Deck", BLACK)    
-    screen.blit(deck_text, (55, 250))
-    crib_text, rect = GAME_FONT.render("Crib", BLACK)    
-    screen.blit(crib_text, (250, 250))
     
-def back_card(x,y):
-    screen.blit(back_card_img, (x,y))
+    deck_text, rect = GAME_FONT.render("Deck", WHITE)    
+    screen.blit(deck_text, (70, 250))
+    screen.blit(back_card_img, (10, 10))
 
-x = 10
-y = 10
+    crib_text, rect = GAME_FONT.render("Crib", WHITE)    
+    screen.blit(crib_text, (250, 250))
+    screen.blit(back_card_img, (190, 10))
+
+    sum_text, rect = GAME_FONT.render("Sum: ", WHITE)    
+    screen.blit(sum_text, (550, 30))
+
+    # Player 1
+    player_1_text, rect = GAME_FONT.render("Player 1:", WHITE)    
+    screen.blit(player_1_text, (760, 30))
+
+    # Player 2
+    player2_text, rect2 = GAME_FONT.render("Player 2:", WHITE)
+    screen.blit(player2_text, (760, 60))
+
+def place_hand(image_array):
+    x, y = 10, 470
+    for img in image_array:
+        screen.blit(img, (x, y))
+        x+=100
+
+def place_player_one(card_img):
+    screen.blit(card_img, (550,170))
+
+def place_player_two(card_img):
+    screen.blit(card_img, (760,170))
+
+def empty_crib():
+    screen.blit(empty_img, (198,48))
+
+def cut_deck(img):
+    screen.blit(img, (10, 10))
+
+def player_turn(player_number):
+    if player_number == 1:
+        pygame.draw.circle(screen, RED, (745, 40), 5)
+    elif player_number == 2:
+        pygame.draw.circle(screen, RED, (745, 70), 5)
+    else:
+        print("player_turn receives", player_number)
 
 while not crashed:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             crashed = True
-    default_screen()
-    back_card(x,y)
-    back_card(x,y+470)
-    back_card(x+100,y+470)
-    back_card(x+200,y+470)
-    back_card(x+300,y+470)
-    back_card(x+400,y+470)
-    back_card(x+500,y+470)
-    
 
-    back_card(x+180,y)
+     #  Testing
 
-    text_surface3, rect = GAME_FONT.render("Sum: 420", WHITE)    
-    screen.blit(text_surface3, (650, 30))
+    default_screen() 
+    image_array = [back_card_img, back_card_img, back_card_img, back_card_img, back_card_img, back_card_img]
+    place_hand(image_array)
+    place_player_one(back_card_img)
+    place_player_two(back_card_img)
+    empty_crib()
+    player_turn(1)
+    player_turn(2)
 
-    #placed down stack
-    back_card(x+600,y+220)
-    back_card(x+780,y+220)
-
-
-
-    # Player 1
-    text_surface, rect = GAME_FONT.render("Player 1 (20)", BLUE)    
-    screen.blit(text_surface, (800, 30))
+    '''
+    Player 1
     pygame.draw.circle(screen, WHITE, (1024, 625), 5)
     pygame.draw.circle(screen, WHITE, (1024, 638), 5)
 
-    # Player 2
-    text_surface2, rect2 = GAME_FONT.render("Player 2 (60)", RED)
-    screen.blit(text_surface2, (800, 60))
+    Player 2
     pygame.draw.circle(screen, WHITE, (1069, 625), 5)
     pygame.draw.circle(screen, WHITE, (1069, 638), 5)
-
+    '''
     pygame.display.update()
 
 pygame.quit()
