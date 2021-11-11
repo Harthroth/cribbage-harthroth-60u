ca5972cf46f8587b5cb76c6bfe5a1ec922be6016
Luis <77930041+60u@users.noreply.github.com>
2021-11-11 08:03:49 -0600

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,6 +1,6 @@
 import pygame
 from math import pi
-import pygame.freetype  # Import the freetype module.
+import pygame.freetype
 
 pygame.init()
 
@@ -43,17 +43,6 @@ def default_screen():
     screen.blit(crib_text, (250, 250))
     screen.blit(back_card_img, (190, 10))
 
-    sum_text, rect = GAME_FONT.render("Sum: ", WHITE)    
-    screen.blit(sum_text, (550, 30))
-
-    # Player 1
-    player_1_text, rect = GAME_FONT.render("Player 1:", WHITE)    
-    screen.blit(player_1_text, (760, 30))
-
-    # Player 2
-    player2_text, rect2 = GAME_FONT.render("Player 2:", WHITE)
-    screen.blit(player2_text, (760, 60))
-
 def place_hand(image_array):
     x, y = 10, 470
     for img in image_array:
@@ -78,7 +67,95 @@ def player_turn(player_number):
     elif player_number == 2:
         pygame.draw.circle(screen, RED, (745, 70), 5)
     else:
-        print("player_turn receives", player_number)
+        print("Error: player_turn receives", player_number)
+
+def top_curved_arc(player, points):
+    multiplier = 1
+    if player == 1:
+        multiplier = 0.75
+    if points <= 40:
+        if points == 36:
+            x, y = 1070, 138
+        elif points == 37:
+            x, y = 1076, 122
+        elif points == 38:
+            x, y =  1086, 108
+        elif points == 39:
+            x, y = 1100, 99
+        elif points == 40:
+            x, y = 1116, 95
+        else:    
+            print("Error: " + points) 
+    else:
+        temp_width = 1.76*display_width
+        if points == 45:
+            x, y = temp_width-1070, 138
+        elif points == 44:
+            x, y = temp_width-1076, 122
+        elif points == 43:
+            x, y = temp_width-1086, 108
+        elif points == 42:
+            x, y = temp_width-1100, 99
+        elif points == 41:
+            x, y = temp_width-1116, 95
+        else:
+            print("Error: " + points)
+    return x, y
+
+def board_scoring(player_number, points):
+    y = 597
+    if player_number == 1:
+        x = 1024
+        if points == 0:
+            pygame.draw.circle(screen, WHITE, (1024, 638), 5)          
+        elif points <= 35:
+            temp = points - 1
+            y -= temp * 13
+        elif points <= 45:
+            xy_tuple = top_curved_arc(1, points)
+            x, y = xy_tuple[0], xy_tuple[1]
+        elif points <= 80:
+            x = 1226
+            temp = 35 - (points - 50)
+            y -= temp * 13
+        elif points <= 85:
+            print("ill do this one later")
+        elif points <= 120:
+            x = 1102
+            temp = points - 86
+            y -= temp * 13
+        else:
+            x, y = 1126, 130
+        pygame.draw.circle(screen, WHITE, (x, y), 5)                    
+    elif player_number == 2:
+        x = 1069
+        if points == 0:
+            pygame.draw.circle(screen, WHITE, (1024, 638), 5)            
+        elif points <= 35:
+            temp = points - 1
+            y -= temp * 13
+        elif points <= 45:
+            xy_tuple = top_curved_arc(2, points)
+            x, y = xy_tuple[0], xy_tuple[1]
+        elif points <= 80:
+            x = 1183
+            temp = 35 - (points - 50)
+            y -= temp * 13
+        elif points <= 85:
+            print("ill do this one later")
+        elif points <= 120:
+            x = 1148
+            temp = points - 86
+            y -= temp * 13
+        else:
+            x, y = 1126, 130
+        pygame.draw.circle(screen, WHITE, (x, y), 5)   
+    else:
+        pygame.draw.circle(screen, WHITE, (1024, 625), 5)
+        pygame.draw.circle(screen, WHITE, (1024, 638), 5)
+
+        pygame.draw.circle(screen, WHITE, (1069, 625), 5)
+        pygame.draw.circle(screen, WHITE, (1069, 638), 5)
 
 while not crashed:
     for event in pygame.event.get():
@@ -88,22 +165,39 @@ while not crashed:
      #  Testing
     
     image_array = [back_card_img, back_card_img, back_card_img, back_card_img, back_card_img, back_card_img]
+    
     default_screen() 
+
+    board_scoring(1, 45)
+    board_scoring(1, 44)
+    board_scoring(1, 43)
+    board_scoring(1, 42)
+    board_scoring(1, 41)
+    board_scoring(1, 40)
+    board_scoring(1, 39)
+    board_scoring(1, 38)
+    # + 6, - 16
+    # + 10, -14
+    # + 14, -9
+    '''
+    sum_text, rect = GAME_FONT.render("Sum: ", WHITE)    
+    screen.blit(sum_text, (550, 30))
+
+    # Player 1
+    player_1_text, rect = GAME_FONT.render("Player 1:", WHITE)    
+    screen.blit(player_1_text, (760, 30))
+
+    # Player 2
+    player2_text, rect2 = GAME_FONT.render("Player 2:", WHITE)
+    screen.blit(player2_text, (760, 60))
+
     place_hand(image_array)
     place_player_one(back_card_img)
     place_player_two(back_card_img)
     empty_crib()
     player_turn(1)
     player_turn(2)
-
-    '''
-    Player 1
-    pygame.draw.circle(screen, WHITE, (1024, 625), 5)
-    pygame.draw.circle(screen, WHITE, (1024, 638), 5)
-
-    Player 2
-    pygame.draw.circle(screen, WHITE, (1069, 625), 5)
-    pygame.draw.circle(screen, WHITE, (1069, 638), 5)
+    board_scoring(0,0)
     '''
     pygame.display.update()
 
