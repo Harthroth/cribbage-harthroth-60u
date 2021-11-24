269cccfb93236c27439d205da1c368d04856f82d
Harthroth <38116988+Harthroth@users.noreply.github.com>
2021-11-23 18:52:54 -0600

    Update Cribbage.py

--- Cribbage.py
+++ Cribbage.py
@@ -57,43 +57,43 @@ class Crib_Functions:
         global deck_ID
         response1 = requests.get("https://deckofcardsapi.com/api/deck/"+deck_ID+"/pile/"+player1name+"/add/?count=6")
         response2 = requests.get("https://deckofcardsapi.com/api/deck/"+deck_ID+"/pile/"+player2name+"/add/?count=6")
-        return json.loads(response1), json.loads(response2)
+        return response1.json(), response2.json()
 
 # Creates the crib pile out of 4 cards contributed by the players
     def make_crib(self, list_of_cards):
         response = requests.get("https://deckofcardsapi.com/api/deck/"+self.get_deck_ID+"/pile/crib/add/?cards="+list_of_cards)
-        return json.loads(response)
+        return response.json()
     
 # Draws a card from a specific pile
     def draw_card_from_pile(self, pile_name):
         response = requests.get("https://deckofcardsapi.com/api/deck/"+self.get_deck_ID+"/pile/"+pile_name+"/draw/?count=1")
-        return json.loads(response)
+        return response.json()
 
 # Draws a specific card from a specific pile
     def draw_specific_card_from_pile(self, pile_name, card_name):
         response = requests.get("https://deckofcardsapi.com/api/deck/"+self.get_deck_ID+"/pile/"+pile_name+"/draw/?cards="+card_name)
-        return json.loads(response)
+        return response.json()
 
 # Draws a card from the deck (for top card or turn determination at beginning)
     def draw_card_from_deck(self):
         response = requests.get("https://deckofcardsapi.com/api/deck/"+self.get_deck_ID+"/draw/?count=1")
-        return json.loads(response)
+        return response.json()
 
 # For round play, placing cards in play pile - pile_nameplay
     def add_card_to_play_piles(self, pile_name, card_name):
         response = requests.get("https://deckofcardsapi.com/api/deck/"+deck_ID+"/pile/"+pile_name+"play/add/?cards="+card_name)
-        return json.loads(response)
+        return response.json()
 
 # End of play, put cards in new deck - pile_namescore
     def add_cards_to_score_pile(self, pile_name):
         response1 = requests.get("https://deckofcardsapi.com/api/deck/"+deck_ID+"/pile/"+pile_name+"play/list")
-        x = json.loads(response1)
+        x = response1.json()
         card_list = ""
         for cards in x["piles"][pile_name+'play']["cards"]:
             card_list += cards["code"]+","
         card_list = card_list[:len(card_list)-1]
         response2 = requests.get("https://deckofcardsapi.com/api/deck/"+deck_ID+"/pile/"+pile_name+"score/add/?cards="+card_list)
-        return json.loads(response2)
+        return response2.json()
     
     def get_pile_list(self, pile_name):
         response = requests.get("https://deckofcardsapi.com/api/deck/"+self.get_deck_ID()+"/pile/"+pile_name+"/list/")
